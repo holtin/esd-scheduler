21fef7d1be1261681bd79ba4b827c1bd0a6dce48
javier <javier.hui@gmail.com>
2020-08-11 14:40:43 +0800

    seems ok

--- public/client.js
+++ public/client.js
@@ -1,5 +1,6 @@
 console.log('client running');
 document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpenSubPCC").click();
 
 function printFile(fileName) {
     console.log("issued command to print " + fileName);
@@ -13,6 +14,29 @@ function openLocationPage(pageName, element) {
     }
 
     navLinks = document.getElementsByClassName("nav-link");
+    for (i = 0; i < navLinks.length; i++) {
+        navLinks[i].style.fontWeight = 400;
+        navLinks[i].style.borderWidth = "0px";
+        navLinks[i].classList.remove("active");
+    }
+
+    document.getElementById(pageName).style.display = "block";
+    element.classList.add("active");
+    element.style.fontWeight = 700;
+    element.style.borderWidth = "10px";
+
+    if (pageName == "pcc") document.getElementById("defaultOpenSubPCC").click();
+    if (pageName == "scc") document.getElementById("defaultOpenSubSCC").click();
+}
+
+function openSubLocationPage(pageName, element) {
+    let i, locationContent, navLinks;
+    locationContent = document.getElementsByClassName("location-subcontent");
+    for (i = 0; i < locationContent.length; i++) {
+        locationContent[i].style.display = "none";
+    }
+
+    navLinks = document.getElementsByClassName("sub-nav-link");
     for (i = 0; i < navLinks.length; i++) {
         navLinks[i].style.fontWeight = 400;
         navLinks[i].style.borderWidth = "0px";
@@ -56,62 +80,70 @@ $("#datepicker-submit").on("click", function () {
             document.getElementById("forms").innerHTML += feedback;
         }
 
+
+        document.getElementById("pcc-v5").innerHTML = '';
+        document.getElementById("pcc-vrms").innerHTML = '';
+        document.getElementById("pcc-pps").innerHTML = '';
+        document.getElementById("scc-v5").innerHTML = '';
+        document.getElementById("scc-vrms").innerHTML = '';
+        document.getElementById("scc-pps").innerHTML = '';
+
         let SCC = data.location[0].SCC;
         let PCC = data.location[1].PCC;
         let feedback = '';
-        let empty = false;
         for (let i = 0; i < PCC.length; ++i) {
+            feedback = '';
             let key = Object.keys(PCC[i])[0];
             if (PCC[i][key].length == 0) continue;
             feedback += '<div class="location-box my-3 px-3 py-3">';
-            feedback += '<h5>' + Object.keys(PCC[i])[0] + '</h5>';
+            feedback += '<h5>' + key + '</h5>';
             for (let j = 0; j < PCC[i][key].length; ++j) {
                 if (typeof PCC[i][key][j] === 'string') {
                     if (j == 0) feedback += '<ul>';
-                    feedback += '<li>' + PCC[i][key][j] + '</li>';
+                    feedback += '<li class="border">' + PCC[i][key][j] + '</li>';
                     if (j == PCC[i][key].length - 1) feedback += '</ul>';
                 }
                 else {
                     if (j == 0) feedback += '<div class="row">';
                     feedback += '<div class="col-md"><ul>';
                     for (let k = 0; k < PCC[i][key][j].length; ++k) {
-                        feedback += '<li>' + PCC[i][key][j][k] + '</li>';
+                        feedback += '<li class="border">' + PCC[i][key][j][k] + '</li>';
                     }
                     feedback += '</ul></div>';
                     if (j == PCC[i][key].length - 1) feedback += '</div>';
                 }
             }
             feedback += '</div>';
+            if (key.includes("VALID V")) document.getElementById("pcc-v5").innerHTML += feedback;
+            if (key.includes("VRMS")) document.getElementById("pcc-vrms").innerHTML += feedback;
+            if (key.includes("PPS")) document.getElementById("pcc-pps").innerHTML += feedback;
         }
-        if (empty) feedback += '<h5 class="text-center">No tapes here</h5>';
-        document.getElementById("pcc").innerHTML = feedback;
-        feedback = '';
-        empty = true;
         for (let i = 0; i < SCC.length; ++i) {
+            feedback = '';
             let key = Object.keys(SCC[i])[0];
             if (SCC[i][key].length == 0) continue;
-            empty = false;
             feedback += '<div class="location-box my-3 px-3 py-3">';
             feedback += '<h5>' + Object.keys(SCC[i])[0] + '</h5>';
             for (let j = 0; j < SCC[i][key].length; ++j) {
                 if (typeof SCC[i][key][j] === 'string') {
                     if (j == 0) feedback += '<ul>';
-                    feedback += '<li>' + SCC[i][key][j] + '</li>';
+                    feedback += '<li class="border">' + SCC[i][key][j] + '</li>';
                     if (j == SCC[i][key].length - 1) feedback += '</ul>';
                 }
                 else {
                     if (j == 0) feedback += '<div class="row">';
                     feedback += '<div class="col-md"><ul>';
                     for (let k = 0; k < SCC[i][key][j].length; ++k) {
-                        feedback += '<li>' + SCC[i][key][j][k] + '</li>';
+                        feedback += '<li class="border">' + SCC[i][key][j][k] + '</li>';
                     }
                     feedback += '</ul></div>';
                     if (j == SCC[i][key].length - 1) feedback += '</div>';
                 }
             }
             feedback += '</div>';
+            if (key.includes("VALID V")) document.getElementById("scc-v5").innerHTML += feedback;
+            if (key.includes("VRMS")) document.getElementById("scc-vrms").innerHTML += feedback;
+            if (key.includes("PPS")) document.getElementById("scc-pps").innerHTML += feedback;
         }
-        if (empty) feedback += '<h5 class="text-center">No tapes here</h5>';
-        document.getElementById("scc").innerHTML = feedback;
     });
 });
